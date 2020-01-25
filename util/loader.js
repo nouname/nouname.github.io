1e673bd2bec8047b43a1122a3a3fb02351e200ef
nouname <darymadry@icloud.com>
2020-01-25 23:39:39 +0300

    update

--- util/loader.js
+++ util/loader.js
@@ -1,9 +1,16 @@
+const params = new URLSearchParams(window.location.href);
+const AC = params.get("https://nouname.github.io/#access_token");
+
 function loadFeed(VK) {
+    if (AC == null)
+        return
     VK.api("newsfeed.get", {"v": "5.73"}, function (data) {
         console.log(data)
     })
 }
 
 function setDataToView(data, viewId) {
+    if (AC == null)
+        return
     document.getElementById(viewId).innerHTML = data;
 }
