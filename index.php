1dbc6f8a52ae5e4f64a4952cdf45f3309c34721f
nouname <45148892+nouname@users.noreply.github.com>
2020-03-02 14:38:23 +0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-echo 'Hello, ' . htmlspecialchars($_POST["name"]) . '!';
+$name=$_POST['name']??'';
+if(empty($name) || !is_string($name)) {
+    http_response_code(400);
+    die("missing POST parameter name!");
+}
+'Привет ' . $name . '!';
 ?>
